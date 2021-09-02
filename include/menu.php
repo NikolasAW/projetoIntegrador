674dd58a943c79d2aad81aa1b35a2bb91efc9569
rhuan <rhuan.oliveira@alunos.sc.senac.br>
2021-09-02 17:05:47 -0300

    foi documentado alterações do site

--- include/menu.php
+++ include/menu.php
@@ -1,20 +1,25 @@
 <div class="row">
     <div class="col-12">
         <div class="d-flex justify-content-center bg-white">
+        <!-- foi inserido o titulo para o site -->
             <h1 class="text-blue my-4 py-2">BROWSE COURSES</h1>
         </div>
     </div>
     <?php
     if (isset($_SESSION['logado'])) :
     ?>
+    <!-- botao para voltar para tela de login -->
         <div>
             <input style="margin-left: 50%;" type="submit" id="sair" value="sair" name="sair"></input>
         </div>
+        <!-- foi inserido o botao sair no site -->
         <?php
         if (isset($_POST["sair"])) {
             session_destroy();
+            // ao clicar no botao o usuario volta para a tela de login e a sessao é destruida
             header('location: cad.php');
         }
         ?>
     <?php endif; ?>
+
 </div>
